89175e97a662b781fbf3445e32b26842055684c4
Pere Joan Martorell <pj.martorell@gmail.com>
2024-07-16 05:34:41 +0200

    Revert "Try Async event-driven processing instead of using threads"
    This reverts commit 2b6d6a8e86f9ca830be5bd68c26366cb62c7e72f.

--- app/controllers/docx_controller.rb
+++ app/controllers/docx_controller.rb
@@ -3,27 +3,17 @@ class DocxController < ApplicationController
     if params[:files].present?
       processed_files = []
 
-      Async do
-        begin
-          tasks = params[:files].map do |file|
-            Async do
-              processed_files << process_docx_file(file)
-            end
-          end
-
-          tasks.each(&:wait) # Wait for all tasks to complete
-
-          # Create a zip file from processed files
-          zip_file_path = create_zip_file(processed_files)
-
-          # Send the zip file for download
-          send_file zip_file_path, type: 'application/zip', disposition: 'attachment', filename: 'processed_files.zip'
-        rescue => e
-          flash[:error] = "Error processing files: #{e.message}"
-          redirect_to root_path
-        ensure
-          processed_files.each { |entry| entry[:temp_file].close! if entry[:temp_file] }
+      begin
+        params[:files].each do |file|
+          processed_file = process_docx_file(file)
+          processed_files << processed_file if processed_file
         end
+
+        # Create a zip file from processed files
+        zip_file_path = create_zip_file(processed_files)
+
+        # Send the zip file for download
+        send_file zip_file_path, type: 'application/zip', disposition: 'attachment', filename: 'processed_files.zip'
       end
     else
       flash[:error] = 'No heu seleccionat cap fitxer 🙃'
@@ -39,20 +29,18 @@ class DocxController < ApplicationController
     # Example processing: Remove the second paragraph if it exists
     doc.paragraphs.at(2).remove! if doc.paragraphs.size > 2
 
-    # Output the processed document as a stream
+    # Save processed content to an in-memory StringIO object using the stream method
     processed_doc_io = doc.stream
 
     { original_filename: file.original_filename, doc_io: processed_doc_io }
-  rescue => e
-    raise "Failed to process #{file.original_filename}: #{e.message}"
   end
 
   def create_zip_file(files)
     zip_file_path = Tempfile.new(%w[processed_files_ .zip]).path
     ::Zip::File.open(zip_file_path, Zip::File::CREATE) do |zip|
       files.each do |entry|
-        zip.get_output_stream(entry[:original_filename]) do |zip_stream|
-          zip_stream.write(entry[:doc_io].read)
+        zip.get_output_stream(entry[:original_filename]) do |stream|
+          stream.write(entry[:doc_io].read) # Write the in-memory doc content directly to the zip
         end
       end
     end
