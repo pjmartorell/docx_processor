d3191f5ecdc8d68fedf2fbb0720f2383fddb951f
Pere Joan Martorell <pj.martorell@gmail.com>
2024-07-17 11:33:53 +0200

    Add border to flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,9 +3,9 @@ module ApplicationHelper
     base_classes = "p-4 rounded mt-4 flash-message"
     case type
     when "notice"
-      "bg-green-200 text-green-800 #{base_classes}"
+      "border border-green-400 bg-green-200 text-green-800 #{base_classes}"
     when "error"
-      "bg-red-200 text-red-800 #{base_classes}"
+      "border border-red-400 bg-red-200 text-red-800 #{base_classes}"
     else
       base_classes
     end
